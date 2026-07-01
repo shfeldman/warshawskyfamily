53932bb6bf2f374762e6022fe40232de796fdc03
Shane Feldman <shanefeldman@Shanes-MacBook-Pro.local>
2026-07-01 09:48:24 -0700

    Add no-cache meta tags; Cloudflare cache fix

--- encrypt.py
+++ encrypt.py
@@ -40,6 +40,9 @@ GATE_HTML = """\
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="0">
 <title>Warshawsky Family Tree</title>
 <style>
   *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
